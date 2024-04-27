19f6bb92e6ecdb057ff12cd8dbc66ff4300921e3
Yassine Oubrhich <youbrhic@e3r7p13.1337.ma>
2024-04-27 23:59:34 +0100

    tha last version

--- butlin/ft_unset.c
+++ butlin/ft_unset.c
@@ -6,35 +6,35 @@
 /*   By: youbrhic <youbrhic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 08:23:24 by aait-bab          #+#    #+#             */
-/*   Updated: 2024/04/26 22:14:25 by youbrhic         ###   ########.fr       */
+/*   Updated: 2024/04/27 23:54:31 by youbrhic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-// void	ft_unset(char **args, char **env)
-// {
-// 	int	i;
-// 	int	j;
-// 	int	len;
+void	ft_unset(char **args, char ***env)
+{
+	int	i;
+	int	j;
+	int	len;
 
-// 	i = 0;
-// 	while (args[++i])
-// 	{
-// 		j = -1;
-// 		len = ft_strlen(args[i]);
-// 		while (env[++j])
-// 		{
-// 			if (!ft_strncmp(args[i], env[j], len) && env[j][len] == '=')
-// 			{
-// 				while (env[j])
-// 				{
-// 					env[j] = env[j + 1];
-// 					j++;
-// 				}
-// 				env[j] = NULL;
-// 				break ;
-// 			}
-// 		}
-// 	}
-// }
+	i = 0;
+	while (args[++i])
+	{
+		j = -1;
+		len = ft_strlen(args[i]);
+		while (env[++j])
+		{
+			if (!ft_strncmp(args[i], env[j], len) && env[j][len] == '=')
+			{
+				while (env[j])
+				{
+					env[j] = env[j + 1];
+					j++;
+				}
+				env[j] = NULL;
+				break ;
+			}
+		}
+	}
+}
